842e81f2bc78e64bae5c50bf71b6084d3a089462
asolermaria <asolermaria@gmail.com>
2026-03-17 15:46:39 +0100

    Done ex 1-3

--- index.js
+++ index.js
@@ -1,11 +1,46 @@
 //DESARROLLA AQUI TUS SOLUCIONES
 
-async function getImageAndName (pokemon){
-
-    let response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
-    let data = await response.json();
-    let name = data.name;
-    let img = data.sprites.front_default;
-    return {name, img}
-    
+//Ejercicio 1
+async function getRandomPokemon() {
+    const randomNumber = Math.floor((Math.random() * 1025) + 1);
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${randomNumber}`);
+    const randomPokemon = await res.json()
+    return randomPokemon
 }
+// getRandomPokemon().then(randomPokemon=>console.log(randomPokemon))
+
+//Ejercicio 2
+async function getImageAndName(pokemon) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`);
+    const pokemonObjeto = await res.json()
+    return {
+        img: pokemonObjeto.sprites.front_default,
+        name: pokemonObjeto.name
+    }
+}
+// getImageAndName().then(pokemon=>console.log(pokemon))
+
+
+// Ejercicio 3
+async function printImageAndName(pokemon) {
+    const res = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemon}`)
+    if (!res.ok) {
+    console.error("Pokemon no encontrado");
+    return `<section>
+            <img src="algo" alt="algo">
+            <h1>algo</h1>
+            </section>`;
+    } else{
+    const conversionPokemon = await res.json()
+    const pokemonObjeto = {
+        img: conversionPokemon.sprites.front_default,
+        name: conversionPokemon.name
+    }
+    return `<section>
+                <img src="${pokemonObjeto.img}" alt="${pokemonObjeto.name}">
+                <h1>${pokemonObjeto.name}</h1>
+            </section>`
+            }
+}
+
+// printImageAndName("pikachu").then(pokemon=>console.log(pokemon))
